84ec51522bb03728c70af0333b3e4987558ba1b7
Andrey <airstern@gmail.com>
2020-05-16 18:05:09 +0300

    Лабораторная работа #1

---
+++ RPTSobjects.WebService/DomainObjects/Ports/IRPTSobjRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace RPTSobj.DomainObjects.Ports
+{
+    public interface IReadOnlyRPTSobjRepository
+    {
+        Task<RPTSobj> GetRPTSobj(long id);
+
+        Task<IEnumerable<RPTSobj>> GetAllRPTSobjs();
+
+        Task<IEnumerable<RPTSobj>> QueryRPTSobj(ICriteria<RPTSobj> criteria);
+
+    }
+
+    public interface IRPTSobjRepository
+    {
+        Task AddRPTSobj(RPTSobj rptsObj);
+
+        Task RemoveRPTSobj(RPTSobj rptsObj);
+
+        Task UpdateRPTSobj(RPTSobj rptsObj);
+    }
+}
