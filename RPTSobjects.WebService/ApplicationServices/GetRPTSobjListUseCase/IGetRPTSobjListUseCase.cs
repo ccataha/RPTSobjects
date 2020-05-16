995d2d41d6eb52d52603de87dfb85649d54f41b4
Andrey <airstern@gmail.com>
2020-05-16 18:08:19 +0300

    Лабораторная работа #3

---
+++ RPTSobjects.WebService/ApplicationServices/GetRPTSobjListUseCase/IGetRPTSobjListUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RPTSobj.ApplicationServices.Interfaces;
+
+namespace RPTSobj.ApplicationServices.GetRPTSobjListUseCase
+{
+    public interface IGetRPTSobjListUseCase : IUseCase<GetRPTSobjListUseCaseRequest, GetRPTSobjListUseCaseResponse>
+    {
+    }
+}
---
+++ RPTSobjects.WebService/ApplicationServices/Ports/Gateways/Database/IRPTSobjDatabaseGateway.cs
@@ -0,0 +1,25 @@
+﻿using RPTSobj.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPTSobj.ApplicationServices.Ports.Gateways.Database
+{
+    public interface IRPTSobjDatabaseGateway
+    {
+        Task AddRPTSobj(DomainObjects.RPTSobj rptsObj);
+
+        Task RemoveRPTSobj(DomainObjects.RPTSobj rptsObj);
+
+        Task UpdateRPTSobj(DomainObjects.RPTSobj rptsObj);
+
+        Task<DomainObjects.RPTSobj> GetRPTSobj(long id);
+
+        Task<IEnumerable<DomainObjects.RPTSobj>> GetAllRPTSobj();
+
+        Task<IEnumerable<DomainObjects.RPTSobj>> QueryRPTSobj(Expression<Func<DomainObjects.RPTSobj, bool>> filter);
+
+    }
+}
