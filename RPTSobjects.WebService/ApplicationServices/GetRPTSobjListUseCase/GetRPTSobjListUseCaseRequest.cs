c147844b95dcbb9364de28b6f4ad7555f546d0c1
Andrey <airstern@gmail.com>
2020-05-16 18:07:20 +0300

    Лабораторная работа #2

---
+++ RPTSobjects.WebService/ApplicationServices/GetRPTSobjListUseCase/GetRPTSobjListUseCaseRequest.cs
@@ -0,0 +1,30 @@
+﻿using RPTSobj.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPTSobj.ApplicationServices.GetRPTSobjListUseCase
+{
+    public class GetRPTSobjListUseCaseRequest : IUseCaseRequest<GetRPTSobjListUseCaseResponse>
+    {
+        public string MetroLine { get; private set; }
+        public long? RPTSobjId { get; private set; }
+
+        private GetRPTSobjListUseCaseRequest()
+        { }
+
+        public static GetRPTSobjListUseCaseRequest CreateAllRPTSobjRequest()
+        {
+            return new GetRPTSobjListUseCaseRequest();
+        }
+
+        public static GetRPTSobjListUseCaseRequest CreateRPTSobjRequest(long rptsObjId)
+        {
+            return new GetRPTSobjListUseCaseRequest() { RPTSobjId = rptsObjId };
+        }
+        public static GetRPTSobjListUseCaseRequest CreateMetroLineRPTSobjRequest(string rptsObjs)
+        {
+            return new GetRPTSobjListUseCaseRequest() { MetroLine = rptsObjs };
+        }
+    }
+}
---
+++ RPTSobjects.WebService/ApplicationServices/GetRPTSobjListUseCase/MetroLineCriteria.cs
@@ -0,0 +1,20 @@
+﻿using RPTSobj.DomainObjects;
+using RPTSobj.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace RPTSobj.ApplicationServices.GetRPTSobjListUseCase
+{
+    public class MetroLineCriteria : ICriteria<DomainObjects.RPTSobj>
+    {
+        public string MetroLine { get; }      /*save filtration criteria*/
+
+        public MetroLineCriteria(string metroLine) /*get criteria and save as class member*/
+            => MetroLine = metroLine;
+
+        public Expression<Func<DomainObjects.RPTSobj, bool>> Filter
+            => (rpts => rpts.MetroLine == MetroLine); /*Filter*/
+    }
+}
